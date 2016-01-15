b2a8151cfaf918c4fac17b1e5cb91b9468276e4b
Sebastian Benz <sbenz@google.com>
2016-01-15 13:53:44 +0000

    extract amp controller

--- app.js
+++ app.js
@@ -16,11 +16,6 @@
 
 var express = require('express');
 var app = express();
-var path = require('path');
-var http = require('http');
-var _ = require('underscore');
-var urlModule = require('url');
-var urlModule = require('url');
 
 var PORT = 8002;
 
@@ -36,97 +31,14 @@ app.enable('view cache');
 app.engine('html', require('hogan-express'));
 app.locals.delimiters = '<% %>';
 
+// Middleware 
 app.use(require('./middlewares/amp-paywall-cors'));
 app.use(require('./middlewares/logging'));
 
+// Controllers
 app.use(require('./controllers/article'));
 app.use(require('./controllers/login'));
-
-ClientAuth = require('./models/client-access');
-User = require('./models/user');
-
-var AUTH_COOKIE_MAX_AGE = 1000 * 60 * 60 * 2; //2 hours
-var ROOT = __dirname;
-var ARCHIVE_ROOT = path.join(ROOT, 'archive');
-
-var MAX_VIEWS = 3;
-
-function cookieJoin(req, clientAuth) {
-  //retrieve the cookie. If it's not null, add a reference to the user on CLIENT_ACCESS[readerId]
-  var email = req.cookies.email;
-  var user = User.findByEmail(email)
-  if (email && user) {
-    clientAuth.user = user;
-  }
-}
-
-/** AUTHORIZATION CORS */
-app.get('/amp-authorization.json', function(req, res) {
-  console.log('Client access verification');
-  var readerId = req.query.rid;
-  if (!readerId) {
-    res.sendStatus(400);
-    return;
-  }
-  var viewedUrl = req.query.url;
-
-  var clientAuth = ClientAuth.getOrCreate(readerId);
-  console.log("viewedUrls: " + Object.keys(clientAuth.viewedUrls).length);
-
-  cookieJoin(req, clientAuth);
-
-  var response;
-  console.log('client auth', clientAuth.user);
-  if (clientAuth.user) {
-    // Subscriber.
-    response = {
-      'access': true
-    };
-  } else {
-    // Metered.
-    var views = Object.keys(clientAuth.viewedUrls).length;
-
-    // Count view if user hasn't already seen the url.
-    if (!(viewedUrl in clientAuth.viewedUrls)) {
-      views += 1;
-    }
-
-    response = {
-      'views': views,
-      'maxViews': MAX_VIEWS,
-      'access': (views <= MAX_VIEWS)
-    };
-  }
-  console.log('Authorization response:', readerId, response);
-  res.json(response);
-});
-
-/** PINGBACK CORS */
-app.post('/amp-pingback', function(req, res) {
-  console.log('Client access pingback');
-  var readerId = req.query.rid;
-  if (!readerId) {
-    res.sendStatus(400);
-    return;
-  }
-
-  var viewedUrl = req.query.url;
-  if (!viewedUrl) {
-    res.sendStatus(400);
-    return;
-  }
-
-  var clientAuth = ClientAuth.getOrCreate(readerId);
-  cookieJoin(req, clientAuth);
-
-  var views = Object.keys(clientAuth.viewedUrls).length;
-  if (!clientAuth.user && views <= MAX_VIEWS) {
-    // Metered.
-    clientAuth.viewedUrls[viewedUrl] = true;
-  }
-  console.log('Pingback response:', readerId, {}, clientAuth);
-  res.json({});
-});
+app.use(require('./controllers/amp-paywall'));
 
 port = process.env.PORT || PORT;
 var server = app.listen(port, function() {
@@ -134,6 +46,4 @@ var server = app.listen(port, function() {
   var port = server.address().port;
 
   console.log('Publisher listening on: ', port);
-  console.log('ROOT: ' + ROOT);
-  console.log('ARCHIVE_ROOT: ' + ARCHIVE_ROOT);
 });
