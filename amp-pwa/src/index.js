b13933dedfa90e765cad730cd40b95897f2c3101
William Chou <willchou@google.com>
2016-09-20 20:14:18 -0400

    bundle amp-shadow to temporarily work around shadow API bug

--- amp-pwa/src/index.js
+++ amp-pwa/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import Shell from './components/shell';
+import './amp-shadow'; // TODO(choumx): Remove once prod bug is fixed.
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './bootstrap-theme.css'; // Replace with your own bootstrap theme!
