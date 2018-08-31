9d5eadf390bea76cd69c1184dda11737100343e6
Jon Newmuis <newmuis@google.com>
2018-08-31 13:51:35 -0400

    Update README and add linting to gulpfile

--- amp-story/README.md
+++ amp-story/README.md
@@ -1,7 +1,16 @@
 # AMP Story
 
-## Practices
+## Development Practices
+
+### Developing for amp-story
+- Follow best practices listed in the [best practices guide](https://www.ampproject.org/docs/fundamentals/amp_story_best_practices).
+- Avoid using `px` units for sizing; instead prefer `em`, `rem`, or `%`.
+
+### Sending a Pull Request
 - Keep pull requests small in size.  Generally each pull request should only
 encompass a single new feature.
-- Before mailing a PR, run `eslint`
-- Ensure that your examples validate
+- Before sending a pull request:
+    - Ensure that your examples are valid amphtml (run `gulp validate`)
+    - Ensure that any configuration JSON (bookend, consent, access, etc.) is
+    valid
+    - Ensure that your code is linted appropriately (run `gulp lint`)
--- amp-story/gulpfile.js
+++ amp-story/gulpfile.js
@@ -1,16 +1,34 @@
 const gulp = require('gulp');
 const gulpAmpValidator = require('gulp-amphtml-validator');
+const gulpStylelint = require('gulp-stylelint');
+const gulpHtmllint = require('gulp-htmllint');
 
 const paths = {
-  templateHtml: 'templates/**/*.htm*'
+  templates: 'templates/**/*.htm*'
 };
 
 gulp.task('validate', () => {
-  return gulp.src(paths.templateHtml)
-    .pipe(gulpAmpValidator.validate())
-    .pipe(gulpAmpValidator.format())
-    .pipe(gulpAmpValidator.failAfterError());
+  return gulp.src(paths.templates)
+      .pipe(gulpAmpValidator.validate())
+      .pipe(gulpAmpValidator.format())
+      .pipe(gulpAmpValidator.failAfterError());
 });
 
-gulp.task('default', ['validate'], function () {
+
+gulp.task('lint-css', () => {
+  return gulp.src(paths.templates)
+      .pipe(gulpStylelint({
+        reporters: [
+          { formatter: 'string', console: true },
+        ]
+      }));
 });
+
+
+gulp.task('lint-html', () => {
+  return gulp.src(paths.templates)
+      .pipe(gulpHtmllint({}));
+});
+
+gulp.task('lint', ['lint-html', 'lint-css'], () => {});
+gulp.task('default', ['validate', 'lint'], () => {});
